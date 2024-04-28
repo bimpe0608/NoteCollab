a62be9a401f83d7656a945b1567b57770d6b4d6f
bimpe <adeyinminuadebimpe@gmail.com>
2024-04-28 22:18:05 +0100

    updated firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,15 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { Firestore, getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { Firestore, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB9pfunhjKILPpl5L7fDEe8FklvDOpWOds',
-  authDomain: 'student-blog-32855.firebaseapp.com',
-  projectId: 'student-blog-32855',
-  storageBucket: 'student-blog-32855.appspot.com',
-  messagingSenderId: '985066312988',
-  appId: '1:985066312988:web:283ba4c6280a04c237d6d9',
-  measurementId: 'G-4GRYBMVNNP',
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
